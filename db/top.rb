0831e0f5bb84e64d7407dc7fdc99614a6c7a15e2
Edward Dal Santo <ejds001@gmail.com>
2016-12-31 15:51:58 -0500

    Remove duplicate database records

--- db/top.rb
+++ db/top.rb
@@ -2,12 +2,13 @@ def register(customers)
   customers.sheets.each do |name|
     customers.sheet(name).each_with_index(customers: 'Name', products: 'Item ID', quantities: 'Qty') do |hash, index| 
       if hash.values.all? { |v| v.to_s != '' } && index != 0
-        #existing = Purchase.where(customer: hash[:customers], product: hash[:products])
-      #  if !existing.empty?
-      #    existing.quantity
-      #  else
-          Purchase.create( customer: hash[:customers], product: hash[:products], quantity: hash[:quantities] )
-       # end
+       record_exists = Purchase.exists?(customer: hash[:customers], product: hash[:products])
+        if record_exists
+          record = Purchase.find_by(customer: hash[:customers], product: hash[:products])
+          record.update(quantity: record.quantity + hash[:quantities])
+        else
+          Purchase.create(customer: hash[:customers], product: hash[:products], quantity: hash[:quantities])
+        end
       end
     end
   end
