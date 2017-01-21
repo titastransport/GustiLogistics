90364beb12619746bb4d49d6ef5aa8eb7abf1a5b
Edward Dal Santo <ejds001@gmail.com>
2017-01-20 18:13:47 -0800

    Set up home page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+
+  def home
+    #if logged_in?
+    #end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   resources :product_imports
   resources :products do 
     collection { post :import }
   end
-
-  root      'products#index'
 
 end
