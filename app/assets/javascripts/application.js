fc9436e42566492d077fcbe23a99ad9d45dcd632
Eddie <ejds001@gmail.com>
2017-01-22 12:30:36 -0800

    Implement basic login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require chartkick
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,14 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false } 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  # Returns the hash digest of the given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   private
 
     def downcase_email
