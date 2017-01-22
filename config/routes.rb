c4f78cc276ae540728691b7941c2c62f119c5270
Edward Dal Santo <ejds001@gmail.com>
2017-01-21 20:11:40 -0800

    working in login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root   'static_pages#home'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :product_imports
   resources :products do 
