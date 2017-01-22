e6e20da1a40c78e52ab34643b863ad236809dd31
Edward Dal Santo <ejds001@gmail.com>
2017-01-21 19:34:49 -0800

    Use SSL and the Puma webserver in product

--- db/migrate/20170121042911_add_emails_to_users.rb
+++ db/migrate/20170121042911_add_emails_to_users.rb
@@ -1,5 +1,5 @@
 class AddEmailsToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :email, :string
+    add_column :users, :email, :string, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121043624) do
+ActiveRecord::Schema.define(version: 20170122032825) do
 
   create_table "product_imports", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170121043624) do
     t.datetime "updated_at",      null: false
     t.string   "email"
     t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
