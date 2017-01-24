04a3eb83f32fa0d4dc3c5003631f264619dc7c75
Eddie <ejds001@gmail.com>
2017-01-23 21:27:09 -0800

    Implement Railscast Calendar; need to fix cell dimensions

--- app/controllers/reorders_controller.rb
+++ app/controllers/reorders_controller.rb
@@ -1,4 +1,7 @@
 class ReordersController < ApplicationController
   def index
+    @reorders = Reorder.all
+    @reorders_by_date = @reorders.group_by(&:date)
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
 end
