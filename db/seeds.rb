b62a52049c2d690d8fe7fab8f9f120657fe966a9
Eddie <ejds001@gmail.com>
2017-01-27 20:23:31 -0800

    Uncomment initializer in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ require_relative 'top'
 #DIR2016 = 'unitactivityreportfaella2016'
 #
 ## use first sheet to initialize products with populate function
-#initializer = Roo::Spreadsheet.open('db/unitactivityreportfaella2015/UAR_January_2015.xlsx')
-#populate(initializer)
+initializer = Roo::Spreadsheet.open('db/unitactivityreportfaella2015/UAR_January_2015.xlsx')
+populate(initializer)
 
 #Dir.foreach(DIR2015) do |file|
 #  uar = Roo::Spreadsheet.open(file)
