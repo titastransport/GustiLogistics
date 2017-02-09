647ee91d15cc0ef8d7a1a7c2fa3d3078525f361f
Edward Dal Santo <ejds001@gmail.com>
2017-02-09 09:28:35 -0800

    Adjust path uar seed files

--- db/seeds/uar/uar2015.seeds.rb
+++ db/seeds/uar/uar2015.seeds.rb
@@ -2,7 +2,7 @@ require_relative '../seed_helper'
 require_relative 'upload_activities'
 
 # Upload all unit activity Faella for 2016
-PATH_TO_DIR = "#{Rails.root}/db/seeds/unit_activity_reports/unitactivityreportfaella2015/"
+PATH_TO_DIR = "#{Rails.root}/db/seeds/uar/unit_activity_reports/unitactivityreportfaella2015/"
 Dir.foreach(PATH_TO_DIR) do |file|
   next if is_hidden?(file)
   path_to_file = "#{PATH_TO_DIR}/#{file}"
