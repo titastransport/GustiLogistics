720519adcffdea3d438cca084f1cf7c4e09f8277
Eddie <ejds001@gmail.com>
2017-02-11 17:25:41 -0800

    Work in more testing for reorder date/quantity

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,7 +1,4 @@
 module ProductsHelper
-  # start with standard 6 months as "Look back" time
-  # May need to update, and may need to keep optimal lookback time for each
-  # product so may need to store these values in database
   SIX_MONTHS = 6
   TWELVE_MONTHS = 12
   DAYS_IN_MONTH = 30
@@ -18,14 +15,8 @@ module ProductsHelper
     Activity.first.date
   end
 
-  # Find all records from last N months from the model associated with Product
-  # Need to find last 12 months and then do first half in one table and second
-  # half in another table
-  # Choosing to get all last 12 months and then divide into last six months
-  # and previous six months, because I'm assuming it's more efficient this way
-  # to go into database only once
-
-  ## works
+  # Finds all records from an associated model between dates with current
+  # product id
   def matching_records(associated_model, start_date, final_date)
     associated_model.where(date: start_date..final_date, product_id: @product.id)
   end
@@ -54,7 +45,7 @@ module ProductsHelper
     final_date = most_recent_date - 6.months
     start_date = final_date - 5.months
 
-    find_top_customers(start_date, final_date)
+    top_customers = find_top_customers(start_date, final_date)
   end
 
   def find_top_customers(start_date, final_date)
@@ -74,7 +65,7 @@ module ProductsHelper
     total_units_sold(start_date, final_date) - total_wholesale_units_sold(wholesale_totals)
   end
 
-    # Finds top n customers
+  # Finds top n customers
   def sort_customers(totals)
     totals.sort_by { |_, quantity| quantity }.reverse!
   end
@@ -92,47 +83,51 @@ module ProductsHelper
     total_units_sold(start_date, final_date) / TWELVE_MONTHS
   end
 
-  def total_wait_time
+  def normal_order_wait_time
     @product.lead_time + @product.travel_time
   end
 
-  def cant_ship_interval
-  # lead_time will be stored as integer or string so using to_f will work
-    # regardless
+  def lead_time_days
+    # lead_time will be stored as integer or string so using to_f will work
     lead_time_days = @product.lead_time.to_f * DAYS_IN_MONTH
-
-    # can't order when within a month of cant ship start
-    cant_ship_start = @product.cant_travel_start.yday - lead_time_days
-    # in contrast, can order when within a month of a cant ship start
-    cant_ship_end = @product.cant_travel_end.yday - lead_time_days
-
-    (cant_ship_start..cant_ship_end)
   end
 
   def months_in_interval(interval)
     interval.end.month - interval.first.month
   end
 
+  def cant_ship_interval
+    # can't order when within a month of cant travel start
+    cant_ship_start = @product.cant_travel_start.yday - lead_time_days
+    # in contrast, can order when within a month of a cant travel start
+    cant_ship_end = @product.cant_travel_end.yday - lead_time_days
+
+    # returns range of integers as dates are reprsented by their yday
+    (cant_ship_start..cant_ship_end)
+  end
+
   def producer_cant_ship_block?
     cant_ship_interval.include?(reorder_date.yday)
   end
 
-  # In yday format, which basically means integers
+  # In yday format, or integer representation of day in 365 days of year
   def cant_produce_interval
-    cant_produce_start = @product.cant_produce_start.yday
+    cant_produce_start = @product.cant_produce_start.yday - lead_time_days 
+    # don't subtract lead time here like in cant_ship because production
+    # affected here but not in cant ship
     cant_produce_end = @product.cant_produce_end.yday
 
     (cant_produce_start..cant_produce_end)
   end
-  
+
   def producer_cant_produce_interval?
     cant_produce_interval.include?(reorder_date.yday)
   end
 
   # Sales that occur in waiting period from time of order to receiving the order
   # physically in warehouse
-  def waiting_sales
-    total_wait_time * average_monthly_sales * growth
+  def naive_waiting_sales
+    normal_order_wait_time * average_monthly_sales * growth
   end
 
   def growth
@@ -141,44 +136,82 @@ module ProductsHelper
 
   # happens essentially when product inventory at 2 months
   def naive_reorder_in
-    inventory_adjusted_for_wait = @product.current - waiting_sales
+    inventory_adjusted_for_wait = @product.current - naive_waiting_sales
 
     ((inventory_adjusted_for_wait / (average_monthly_sales *
-    growth)) * DAYS_IN_MONTH).round(1)
+                                     growth)) * DAYS_IN_MONTH).round(1)
   end
 
-  def update_reorder_date
+  def reorder_date
     Date.today + @product.reorder_in
+  end
+
+  def sooner_cant_order_block
+    [cant_ship_interval.first, cant_produce_interval.first].min
   end
 
   def proper_reorder_in
     if producer_cant_ship_block? && producer_cant_produce_interval?
-      sooner = [cant_ship_interval.first, cant_produce_interval.first].min
-      if Date.today.yday <= sooner
-        sooner - Date.today.yday
+      if Date.today.yday <= sooner_cant_order_block
+        sooner_cant_order_block - Date.today.yday
       else
         last_block = [cant_ship_interval.end, cant_produce_interval.end].max
         last_block - Date.today.yday
       end
-    elsif producer_cant_ship_block? 
-      if Date.today <= cant_produce_interval.first
-        cant_produce_interval.first - Date.today
+    elsif producer_cant_produce_interval? 
+      if Date.today.yday <= cant_produce_interval.first
+        cant_produce_interval.first - Date.today.yday
       else 
-        cant_produce_interval.end - Date.today
+        cant_produce_interval.end - Date.today.yday
       end
     elsif producer_cant_ship_block? 
-      if Date.today <= cant_ship_interval.first
-        cant_ship_interval.first - Date.today
+      if Date.today.yday <= cant_ship_interval.first
+        cant_ship_interval.first - Date.today.yday
       else 
-        cant_ship_interval.end - Date.today
+        cant_ship_interval.end - Date.today.yday
       end
     else
       naive_reorder_in
     end
   end
-  
-  def naive_reorder_quantity
-    ((average_monthly_sales * growth * @product.cover_time).to_i)
+
+  def reorder_quantity
+    if producer_cant_ship_block? && producer_cant_produce_interval?
+      if Date.today.yday <= sooner_cant_order_block
+        full_order - expected_quantity_on_date(sooner_cant_order_block)
+      else
+        full_order
+      end
+    elsif producer_cant_produce_interval? 
+      if Date.today <= cant_produce_interval.first
+        full_order - expected_quantity_on_date(cant_produce_interval.first)
+      else 
+        full_order
+      end
+    elsif producer_cant_ship_block? 
+      if Date.today <= cant_ship_interval.first
+        full_order - expected_quantity_on_date(cant_ship_interval.first)
+      else 
+        full_order
+      end
+    else
+      full_order
+    end
+  end
+
+  def expected_quantity_on_date(date)
+    # need to raise error if date before 
+    days_till = date.yday - Date.today.yday
+    return @product.current if days_till <= 0
+
+    expected_sales_till_date = average_monthly_sales * days_till
+    expected_quantity = @product.current - expected_sales_till_date  
+
+    expected_quantity <= 0 ? 0 : expected_quantity 
+  end
+
+  def full_order
+    (average_monthly_sales * growth * @product.cover_time).to_i
   end
 
   # temporary check if previous product setup while I fill out products
