0e340cc81932806f42faa4ffaf26741cf9578e27
Edward Dal Santo <ejds001@gmail.com>
2017-03-26 15:47:46 -0700

    Refactor import model tests

--- test/models/activity_import_test.rb
+++ test/models/activity_import_test.rb
@@ -14,38 +14,31 @@ describe ActivityImport, '.save' do
     end
 
     it "creates new activities" do
-        num_activities_before = Activity.count 
-
+      assert_difference 'Activity.count', 2 do
         @activity_import.save 
-        num_activities_after = Activity.count
-        
-        num_activities_after.must_be :>, num_activities_before
+      end
     end
 
     it 'updates activities that already exist' do
       @activity_import.save 
-      num_activities_after_first_save = Activity.count 
 
-      @activity_import.save 
-      num_activities_after_second_save = Activity.count
-
-      num_activities_after_first_save.must_be :==, num_activities_after_second_save
+      assert_no_difference 'Activity.count' do 
+        @activity_import.save 
+      end
     end
   end
 
   describe "when given an invalid file" do
 
-    it "rejects activities when a product doesn't exist" do
+    before do
       file = File.new(Rails.root.join('test/fixtures/files/UAR_March_2017_nonexistent_product.xlsx'))
       @activity_import = ActivityImport.new(file: file)
+    end
 
-      num_activities_before = Activity.count 
-
-      @activity_import.save 
-      num_activities_after = Activity.count
-
-
-      num_activities_after.must_be :==, num_activities_before
+    it "rejects activities when a product doesn't exist" do
+      assert_no_difference 'Activity.count' do 
+        @activity_import.save 
+      end
     end
   end
 end
--- test/models/purchase_import_test.rb
+++ test/models/purchase_import_test.rb
@@ -14,39 +14,31 @@ describe PurchaseImport, '.save' do
     end
 
     it "creates new purchases" do
-        num_purchases_before = CustomerPurchaseOrder.count 
-
+      assert_difference 'CustomerPurchaseOrder.count', 3 do
         @purchase_import.save 
-        num_purchases_after = CustomerPurchaseOrder.count
-        
-        num_purchases_after.must_be :>, num_purchases_before
+      end
     end
 
     it 'updates purchases that already exist' do
       @purchase_import.save 
-      num_purchases_after_first_save = CustomerPurchaseOrder.count 
-
-      @purchase_import.save 
-      num_purchases_after_second_save = CustomerPurchaseOrder.count
-
-      num_purchases_after_first_save.must_be :==, num_purchases_after_second_save
+  
+      assert_no_difference 'CustomerPurchaseOrder.count' do
+        @purchase_import.save 
+      end
     end
   end
 
   describe "when given a an invalid file" do
 
-    it "rejects purchases when a product doesn't exist" do
+    before do
       file = File.new(Rails.root.join('test/fixtures/files/ISTC_March_2017_nonexistent_product.xlsx'))
       @purchase_import = PurchaseImport.new(file: file)
-
-      num_purchases_before = CustomerPurchaseOrder.count 
-
-      @purchase_import.save 
-      num_purchases_after = CustomerPurchaseOrder.count
-
-      num_purchases_after.must_be :==, num_purchases_before
     end
 
+    it "rejects purchases when a product doesn't exist" do
+      assert_no_difference 'CustomerPurchaseOrder.count' do
+        @purchase_import.save
+      end
+    end
   end
-
 end
