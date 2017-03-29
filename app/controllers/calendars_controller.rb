29d532299e3e7fc681be801e38bd1fc7c2c6ce93
Edward Dal Santo <ejds001@gmail.com>
2017-03-29 16:15:51 -0700

    Refactor calendars controller

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,15 +2,22 @@ class CalendarsController < ApplicationController
   def index
     @products = Product.select_setup_products 
 
-    @products.each do |product| 
-      unless product.enroute || product.next_reorder_date > Date.today
-        product.update_reorder_date 
-      end
-    end
+    update_overdue_unordered_products
 
     @products_by_reorder_date = @products.group_by(&:next_reorder_date)
+
     # Defaults to today's date to use this month
     # Date parameter changed by clicking arrow in view
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
+
+  private
+
+    def update_overdue_unordered_products
+      @products.each do |product| 
+        unless product.enroute || product.next_reorder_date > Date.today
+          product.update_attribute(:next_reorder_date, Date.today)
+        end
+      end
+    end
 end
